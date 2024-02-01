ddf5dab3d258e6c6f46eb0b53b562fed56772e73
lauralandonlovell <roryboo@gmail.com>
2024-02-01 15:23:31 +0100

    all conditions are met and nested if statements are all working

--- Program.cs
+++ Program.cs
@@ -2,35 +2,35 @@
 int daysUntilExpiration = random.Next(12);
 int discountPercentage = 0;
 
-if (daysUntilExpiration <= 10 && daysUntilExpiration > 0)
+daysUntilExpiration = 0;
+
+if (daysUntilExpiration < 11 && daysUntilExpiration != 0)
 {
-    Console.WriteLine(daysUntilExpiration);
     if (daysUntilExpiration == 1)
     {
-        Console.WriteLine($"Your subscription expires within a day! Renew now and save 20%!");
-        discountPercentage += (20 / 100);
-        Console.WriteLine(discountPercentage);
+        discountPercentage = 20;
+        Console.WriteLine($"Your subscription expires within a day!\nRenew now and save {discountPercentage}%!");
+        Console.WriteLine(daysUntilExpiration);
     }
-    else if (daysUntilExpiration < 6 && daysUntilExpiration > 1)
+    else if (daysUntilExpiration < 6)
     {
-        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save 10%");
-        discountPercentage += 10 / 100;
-        Console.WriteLine(discountPercentage);
-    } 
-    else
+        discountPercentage = 10;
+        Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.\nRenew now and save {discountPercentage}%!");
+        Console.WriteLine(daysUntilExpiration);
+    }
+    else if (daysUntilExpiration >= 6)
     {
-        Console.WriteLine($"Your subscription will expire soon.Renew now!");
+        Console.WriteLine($"Your subscription will expire soon. Renew now!");
     }
 }
 else if (daysUntilExpiration == 0)
 {
     Console.WriteLine("Your subscription has expired.");
     Console.WriteLine(daysUntilExpiration);
-}
+} 
 else 
 {
-    Console.WriteLine("else");
-    Console.WriteLine(daysUntilExpiration);
+    //Console.WriteLine("do nothing");
 }
 
 
