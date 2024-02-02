1954594eb261106764eee85c481d18bb2e128e8c
lauralandonlovell <roryboo@gmail.com>
2024-02-02 11:13:39 +0100

    finished interation exercise

--- Program.cs
+++ Program.cs
@@ -1,16 +1,44 @@
-﻿string[] fraudulentOrderIDs = new string[3];
+﻿// // string[] fraudulentOrderIDs = new string[3];
 
-fraudulentOrderIDs[0] = "A123";
-fraudulentOrderIDs[1] = "B456";
-fraudulentOrderIDs[2] = "C789";
-//fraudulentOrderIDs[3] = "D000";
+// // fraudulentOrderIDs[0] = "A123";
+// // fraudulentOrderIDs[1] = "B456";
+// // fraudulentOrderIDs[2] = "C789";
+// // //fraudulentOrderIDs[3] = "D000";
 
-Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
-Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+// string[] fraudulentOrderIDs = {"A123", "B456", "C789"};
 
-fraudulentOrderIDs[0] = "F000";
-Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
-Console.WriteLine($"{fraudulentOrderIDs[0]} {fraudulentOrderIDs[1]} {fraudulentOrderIDs[2]}");
+// Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+// Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+// fraudulentOrderIDs[0] = "F000";
+// Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+// Console.WriteLine($"{fraudulentOrderIDs[0]} {fraudulentOrderIDs[1]} {fraudulentOrderIDs[2]}");
+
+// Console.WriteLine($"There are {fraudulentOrderIDs.Length} fraudulent orders processing.");
+
+// using System.Data;
+
+// Console.WriteLine("Iteration Exercise");
+// string[] names = {"Rowena", "Robin", "Bao"};
+// foreach (string name in names)
+// {
+//     Console.WriteLine(name);
+// }
+
+// int[] warehouse = {200, 450, 700, 175, 250};
+// int bin = 0;
+// int sum = 0;
+// foreach (int items in warehouse)
+// {
+//     //Console.WriteLine(items);
+//     bin++;
+//     sum += items;
+//     Console.WriteLine($"In bin {bin} we have {items} items. Running Total: {sum}");
+
+// }
+// Console.WriteLine($"We have {sum} items in the warehouse.");
+
+
 
 
