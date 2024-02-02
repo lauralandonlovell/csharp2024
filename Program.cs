bd4b25cbed091330d3f48f7eacc5892fbc8f73ae
lauralandonlovell <roryboo@gmail.com>
2024-02-02 09:34:01 +0100

    checking to make sure 1st element was reassigned

--- Program.cs
+++ Program.cs
@@ -5,3 +5,12 @@ fraudulentOrderIDs[1] = "B456";
 fraudulentOrderIDs[2] = "C789";
 //fraudulentOrderIDs[3] = "D000";
 
+Console.WriteLine($"First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"Second: {fraudulentOrderIDs[1]}");
+Console.WriteLine($"Third: {fraudulentOrderIDs[2]}");
+
+fraudulentOrderIDs[0] = "F000";
+Console.WriteLine($"Reassign First: {fraudulentOrderIDs[0]}");
+Console.WriteLine($"{fraudulentOrderIDs[0]} {fraudulentOrderIDs[1]} {fraudulentOrderIDs[2]}");
+
+
