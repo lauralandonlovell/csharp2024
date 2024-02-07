d5c18eb4f5a44b5b5dbbe3855886fa70b09d1cdb
lauralandonlovell <roryboo@gmail.com>
2024-02-07 13:46:33 +0100

    build not failing but not there yet

--- Program.cs
+++ Program.cs
@@ -1,7 +1,17 @@
-﻿string[] names = {"Sophia", "Nicolas", "Zahirah", "Jeong"};
+﻿string[] student = {"Sophia", "Andrew", "Emma", "Logan"};
+decimal[] examScore = {92.2m, 89.6m, 85.6m, 91.2m};
+string[] letterGrade = {"A","A-"};
+decimal[] overallGrade = {95.88m, 91.38m, 90.94m, 93.12m};
+decimal[] extraCredit = {(92 + 3.68m), (89 + 1.78m), (89 + 5.34m), (96 + 1.92m)};
+
 int i = 0;
-foreach (string name in names)
-{
-   Console.WriteLine(name);
-}
+foreach (string student1 in student)
+ {
+    Console.WriteLine($"Studen\t\tExam Score\t\tOverall Grade\tExtra Credit");
+    foreach (decimal examScore1 in examScore)
+    {
+        Console.WriteLine($"{student1[0]}:\t\t{examScore[0]}\t\t\t{overallGrade[0]}\t{letterGrade[0]}\t\t{extraCredit[0]}");
+    }
+ }   
 i++;
+Console.WriteLine(i);
