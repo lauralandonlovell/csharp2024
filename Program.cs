4f105312a712e2f0f27adb1fb2aad5b483b11f1d
lauralandonlovell <roryboo@gmail.com>
2024-02-07 08:26:39 +0100

    fixing syntax errors

--- Program.cs
+++ Program.cs
@@ -7,30 +7,30 @@ int[] jeongScores = {90, 92, 98, 100, 97};
 
 string[] letterGrade = {"A", "B", "C", "D", "F"};
 int i = 0;
-foreach (name in names)
+foreach (string name in names)
 {
    Console.WriteLine($"{name}: ");
 }
-    foreach (score in sophiaScores && nicolasScores && zahirahScores && jeongScores)
-    {
-        if (score >= 90)
-        {
-            Console.WriteLine($"{letterGrade[0]}");
-        }
-        elseif (score >= 80)
-        {
-            Console.WriteLine($"{letterGrade[1]}");
-        }
-        elseif (score >= 70)
-        {
-            Console.WriteLine($"{letterGrade[2]}");
-        }
-        elseif
-        {
-            Console.WriteLine($"Your grade is a D or below. Ask for help and keep studying!");
-        }
+    // foreach (score in sophiaScores && nicolasScores && zahirahScores && jeongScores)
+    // {
+    //     if (score >= 90)
+    //     {
+    //         Console.WriteLine($"{letterGrade[0]}");
+    //     }
+    //     elseif (score >= 80)
+    //     {
+    //         Console.WriteLine($"{letterGrade[1]}");
+    //     }
+    //     elseif (score >= 70)
+    //     {
+    //         Console.WriteLine($"{letterGrade[2]}");
+    //     }
+    //     elseif
+    //     {
+    //         Console.WriteLine($"Your grade is a D or below. Ask for help and keep studying!");
+    //     }
         i++;
-    }
+    //}
 
 
 
