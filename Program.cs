4b39336cba93ed4b6a10936fb47a86d8bbb6703a
lauralandonlovell <roryboo@gmail.com>
2024-02-10 04:42:58 +0100

    fake order challenge using StartsWith() method

--- Program.cs
+++ Program.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System.Runtime.CompilerServices;
+
+string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
+
+foreach (string item in orderIDs)
+{
+  if (item.StartsWith("B"))
+  {
+   Console.WriteLine(item);
+  } 
+}
