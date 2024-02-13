56f45e5cf6433e9d24053c6dbae99ef3e8ef3887
lauralandonlovell <roryboo@gmail.com>
2024-02-13 11:11:39 +0100

    fixed iteration problem by removing semicolon after for loop conditions

--- Program.cs
+++ Program.cs
@@ -2,7 +2,7 @@
 
 Console.WriteLine("Generating 10 random numbers:");
 
-for (int ctr = 1; ctr <= 10; ctr++);
+for (int ctr = 1; ctr <= 10; ctr++)
 {
   Console.WriteLine($"{rnd.Next(), 15:N0}");
 }
