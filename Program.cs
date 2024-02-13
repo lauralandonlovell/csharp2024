970180da6467a843f28ec30b94468bcd058c2b93
lauralandonlovell <roryboo@gmail.com>
2024-02-13 08:34:07 +0100

    starting .NET libraries project

--- Program.cs
+++ Program.cs
@@ -1,17 +1,3 @@
-﻿using System.Runtime.CompilerServices;
-
-string[] orderIDs = {"B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179"};
-
-foreach (string item in orderIDs)
-{
-  if (item.StartsWith("B"))
-  {
-      Console.WriteLine(item);
-  }
-  else if (!item.StartsWith("B"))
-  {
-   Console.WriteLine($"OrderID {item} is not a fraudulent orderID.");
-  }
-}
+﻿
 
 
