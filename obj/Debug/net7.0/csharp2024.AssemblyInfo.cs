517fe213bf9c854da9b0c1ec7b3869fb7f1367ef
lauralandonlovell <roryboo@gmail.com>
2024-02-19 19:44:40 +0100

    adjs;klfghios

--- obj/Debug/net7.0/csharp2024.AssemblyInfo.cs
+++ obj/Debug/net7.0/csharp2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+228c5a572b61eaa4f2bcfd18b07c8a9efd488bdc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+daa7515495134728a648202cebc7115f6197de53")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
