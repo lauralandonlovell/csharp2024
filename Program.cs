228c5a572b61eaa4f2bcfd18b07c8a9efd488bdc
lauralandonlovell <roryboo@gmail.com>
2024-02-19 09:14:35 +0100

    received expected compilation error

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
 ﻿bool flag = true;
+int value;
 if (flag)
 {
-    int value = 10;
+    // int value = 10;
     Console.WriteLine($"Inside the code block: {value}");
 }
+value = 10;
 Console.WriteLine($"Outside the code block: {value}");
 
 
--- obj/Debug/net7.0/csharp2024.AssemblyInfo.cs
+++ obj/Debug/net7.0/csharp2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6ba9299c8f95e7c7743cb069aca6476c1ddc6d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc8cb1c9f37aa64f7e61cbfac3c787d72c2f992d")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
