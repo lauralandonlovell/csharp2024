a6ba9299c8f95e7c7743cb069aca6476c1ddc6d2
lauralandonlovell <roryboo@gmail.com>
2024-02-19 08:53:06 +0100

    deleted existing code

--- Program.cs
+++ Program.cs
@@ -1,8 +1,4 @@
-﻿int firstValue = 500;
-int secondValue = 600;
-int largerValue = Math.Max(firstValue, secondValue);
-
-Console.WriteLine($"The larger of the two values is {largerValue}.");
+﻿
 
 
 
