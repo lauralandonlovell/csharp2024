169fc51dc483a89e302c2b385bbd0b38dd532b94
lauralandonlovell <roryboo@gmail.com>
2024-02-19 08:52:11 +0100

    deleted all extraneous program.cs file versions

--- obj/Debug/net7.0/csharp2024.AssemblyInfo.cs
+++ obj/Debug/net7.0/csharp2024.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77fb2a476841074b775c2cc9a75531cf96c8a105")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f444916f554c65a677b3751dc4499d0b5567bcf6")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp2024")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
