3a36bb90182ef9d059062c1cdf797043a2adef10
lauralandonlovell <roryboo@gmail.com>
2024-03-11 10:47:05 +0100

    roleplay game challenge

--- Program.cs
+++ Program.cs
@@ -1,11 +1,42 @@
-﻿for (int i = 1; i < 101; i++)
-   {
-      if ((i % 3 == 0) && (i % 5 == 0))
-        Console.WriteLine($"{i} FizzBuzz");
-      else if (i % 3 == 0)
-        Console.WriteLine($"{i} Fizz");
-      else if (i % 5 == 0)
-        Console.WriteLine($"{i} Buzz");
-      else 
-        Console.WriteLine($"{i}");        
-   }
+﻿using System.IO.Compression;
+
+Random random = new();
+int roll = random.Next(1, 11);
+int monsterHealth = 10;
+int heroHealth = 10;
+
+do
+{
+    roll = random.Next(1, 11);
+    Console.WriteLine(roll);
+    monsterHealth = 10;
+    monsterHealth = monsterHealth - roll;
+    Console.WriteLine(monsterHealth);
+
+} while (monsterHealth > 0);
+{
+    roll = random.Next(1, 11);
+    Console.WriteLine(roll);
+    heroHealth = 10;
+    heroHealth -= roll;
+    Console.WriteLine(heroHealth);
+}
+Console.WriteLine($"Monster Health: {monsterHealth}");
+Console.WriteLine($"Hero Health: {heroHealth}");
+// if (heroHealth > monsterHealth)
+// {
+//     Console.WriteLine($"Hero has won by {heroHealth - monsterHealth} points!");
+// }
+// else
+// {
+//     Console.WriteLine($"Monster wins by {monsterHealth - heroHealth} points!");
+// }
+
+
+
+
+
+
+
+
+
