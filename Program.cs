c3f4509d64d4eec6cf6b13b54926d1fac45cb703
lauralandonlovell <roryboo@gmail.com>
2024-03-18 05:27:55 +0100

    latest update

--- Program.cs
+++ Program.cs
@@ -1,35 +1,47 @@
 ﻿using System.IO.Compression;
 
 Random random = new();
-int roll = random.Next(1, 11);
+int monsterRoll = random.Next(1,11);
+int heroRoll = random.Next(1,11);
 int monsterHealth = 10;
 int heroHealth = 10;
 
 do
 {
-    roll = random.Next(1, 11);
-    Console.WriteLine(roll);
+    heroRoll = random.Next(1, 11);
+    Console.WriteLine(heroRoll);
     monsterHealth = 10;
-    monsterHealth = monsterHealth - roll;
+    monsterHealth = monsterHealth - heroRoll;
     Console.WriteLine(monsterHealth);
 
-} while (monsterHealth > 0);
-{
-    roll = random.Next(1, 11);
-    Console.WriteLine(roll);
+    monsterRoll = random.Next(1, 11);
+    Console.WriteLine(monsterRoll);
     heroHealth = 10;
-    heroHealth -= roll;
+    heroHealth = heroHealth - monsterRoll;
     Console.WriteLine(heroHealth);
-}
+
+} while (monsterHealth > 0 && heroHealth > 0);
+// {
+//     roll = random.Next(1, 11);
+//     Console.WriteLine(roll);
+//     Console.WriteLine("hello");
+//     heroHealth = 10;
+//     heroHealth -= roll;
+//     Console.WriteLine(heroHealth);
+// }
 Console.WriteLine($"Monster Health: {monsterHealth}");
 Console.WriteLine($"Hero Health: {heroHealth}");
 if (heroHealth > monsterHealth)
 {
     Console.WriteLine($"Hero has won by {heroHealth - monsterHealth} points!");
 }
-else
+else if (monsterHealth > heroHealth)
 {
     Console.WriteLine($"Monster wins by {monsterHealth - heroHealth} points!");
+}
+else
+{
+    Console.WriteLine($"Monster and Hero are tied.");
 }
 
 
